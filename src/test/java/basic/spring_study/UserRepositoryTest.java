aa931bfa9bbecb2c161c55837a2a1f0ee79d6069
hyee0715 <hyee0715@naver.com>
2022-03-18 19:37:30 +0900

    CRUD test

--- src/test/java/basic/spring_study/UserRepositoryTest.java
+++ src/test/java/basic/spring_study/UserRepositoryTest.java
@@ -1,16 +1,20 @@
 package basic.spring_study;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Slf4j
 public class UserRepositoryTest extends SpringStudyApplicationTests {
     @Autowired
     private UserRepository userRepository;
 
+    // create test
     @Test
     public void create() {
         // User 생성
@@ -27,5 +31,55 @@ public class UserRepositoryTest extends SpringStudyApplicationTests {
         User newUser = userRepository.save(user);
 
         log.info(newUser.toString());
+    }
+
+    // read test
+    @Test
+    public void read() {
+        Optional<User> user = userRepository.findById(2L);
+        user.ifPresent(
+                selectUser -> { // ifPresent: 값이 있다면, 가져옴
+                    log.info(selectUser.toString());
+                });
+    }
+
+    // update test
+    @Test
+    public void update() {
+        Optional<User> user = userRepository.findById(2L);
+        user.ifPresent(
+                selectUser -> {
+                    User newUser = User.builder()
+                            .id(selectUser.getId())
+                            .account(selectUser.getAccount())
+                            .phoneNumber(selectUser.getPhoneNumber())
+                            .email("updatedEmail@google.com")
+                            .createdAt(selectUser.getCreatedAt())
+                            .createdBy(selectUser.getCreatedBy())
+                            .updatedAt(LocalDateTime.now())
+                            .updatedBy("TestUser02")
+                            .build();
+                    userRepository.save(newUser);
+                });
+    }
+
+    // delete test
+    @Test
+    @Transactional
+    public void delete() {
+        Optional<User> user = userRepository.findById(1L);
+
+        // User가 있어야 함
+        Assertions.assertTrue(user.isPresent());
+
+        // 있다면 삭제
+        user.ifPresent(
+                selectUser -> {
+                    userRepository.deleteById(selectUser.getId());
+                });
+
+        // 지워졌는지 확인
+        Optional<User> deletedUser = userRepository.findById(1L);
+        Assertions.assertFalse(deletedUser.isPresent());
     }
 }
